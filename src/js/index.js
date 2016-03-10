d556d856cab62ce450eec86fd4ccaa35087f5535
JEFF HENRY <henry_jf@hotmail.com>
2016-03-09 19:21:24 -0500

    start js.  sass header/footer/background colors

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 // Javascript Entry Point
+import etsyData from './items';
+
+// var image = images.map(function(){
+// 	return 
+// })
+ function image(){
+	var pic = document.Images[0].url_170x135;
+	document.getElementbyId()
+}
