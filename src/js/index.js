b2b7ef912dcf472d951c9cbd2924013e2d12d3f1
JEFF HENRY <henry_jf@hotmail.com>
2016-03-09 22:24:47 -0500

    working on header layout

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import etsyData from './items';
 // var image = images.map(function(){
 // 	return 
 // })
- function image(){
-	var pic = document.Images[0].url_170x135;
-	document.getElementbyId()
-}
+//  function image(){
+// 	var pic = document.Images[0].url_170x135;
+// 	document.getElementbyId()
+// }
