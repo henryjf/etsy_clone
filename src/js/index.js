26fa586e5a279e3e35382bfb2244bbd69f6443a3
JEFF HENRY <henry_jf@hotmail.com>
2016-03-11 14:09:28 -0500

    added images, etc to js.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,49 @@
 // Javascript Entry Point
 import etsyData from './items';
+import $ from 'jquery';
+
+
+
+var productArray = etsyData.results;
+
+
+// productArray.forEach(function(product) {
+function paintings(product) {
+
+	// Grab the img url
+	var image = product.Images[0].url_170x135;
+	// console.log(image);
+
+	//Grab the price
+	var price = product.price;
+	// console.log(price);
+
+	// Grab the seller
+	var seller =product.Shop.shop_name;
+	// console.log(seller);
+
+	// Grab the title
+	var titleOfProduct = product.title;
+	// console.log(titleOfProduct);
+
+	// Build my template
+
+	return `
+	<div class="productArray">
+		<div class="product.Images">
+			<img src="${image}" alt="${product.description}">
+		</div>
+		<div class="price">${product.price}</div>
+		<div class="seller">${product.Shop.shop_name}></div>
+		<div class="titleOfProduct">${product.title}></div>
+	</div>`;
+}
+
+productArray.forEach(function(product){
+	$('.main').append(paintings(product));
+});
+
+
 
 // var image = images.map(function(){
 // 	return 
@@ -8,9 +52,18 @@ import etsyData from './items';
 // 	var pic = document.Images[0].url_170x135;
 // 	document.getElementbyId()
 // }
-var painting = function items(title, description) {
- return painting
-};
+// var painting = items.pic(){
 
-document.querySelector()
-// console.log(painting)
+// }
+
+// var titles = results.title;
+// var cost = results.price;
+// var seller = name.Shop;
+
+
+// function painting(pic) {
+// 	var painting = pic.Images[0].url_170x135;
+// 	// return firstPainting;
+// }
+
+// console.log(painting);
